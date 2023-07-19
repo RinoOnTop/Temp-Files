cad8b385fd88ba472f9b3fac7e556c14dcda47d7
RiNo <75165318+RinoOnTop@users.noreply.github.com>
2023-07-19 19:29:24 +0300

    Update distro.sh

--- distro.sh
+++ distro.sh
@@ -4,6 +4,7 @@ ARCH=$([ "$(uname -m)" == "x86_64" ] && echo "amd64" || echo "arm64")
 if [ "${ARCH}" == "amd64" ];
 then
 bash <(curl -s https://raw.githubusercontent.com/RinoOnTop/Vps-EGG/main/RiNo-Vps.sh)
+echo 'Using amd64'
 else 
 echo 'arm64 is not supported yet'
 fi
