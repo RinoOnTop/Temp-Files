3e7a89cfcfe7804a0778547b2cb20481353ca4ff
RiNo <75165318+RinoOnTop@users.noreply.github.com>
2023-08-09 11:22:16 +0300

    Update egg.sh

--- egg.sh
+++ egg.sh
@@ -12,7 +12,7 @@ redback=$(echo -en "\e[41m")
 
 echo "
 ${bold}${lightgreen}===================================================================================
-${bold}${lightblue} STARTING PLEASE WAIT ...
+${bold}${lightblue} STARTING PLEASE WAIT ...=
 ${bold}${lightgreen}===================================================================================
   
  "
@@ -94,13 +94,13 @@ ${bold}${lightred}⛔️  To use this variable, you have to be using docker: ash
     echo "✅ Starting VPS"
     echo "${bold}${lightgreen}==> ${lightblue}Starting${lightgreen} Container <=="
     function runcmd1 {
-        printf "${bold}${lightred}vRoot${nc}@${lightblue}Container${nc}:~ "
+        printf "${bold}${lightgreen}vRoot${nc}@${lightblue}Container${nc}:~ "
         read -r cmdtorun
         $proot  $console $bash "$cmdtorun"
         runcmd
     }
     function runcmd {
-        printf "${bold}${lightred}vRoot${nc}@${lightblue}Container${nc}:~ "
+        printf "${bold}${lightgreen}vRoot${nc}@${lightblue}Container${nc}:~ "
         read -r cmdtorun
         $proot $console $bash "$cmdtorun"
         runcmd1
@@ -217,13 +217,13 @@ ${bold}${lightgreen}============================================================
  
     echo "${bold}${lightgreen}==> ${lightblue}Container${lightgreen} Started <=="
     function runcmd1 {
-        printf "${bold}${lightred}vRoot${nc}@${lightblue}Container${nc}:~ "
+        printf "${bold}${lightred}pRoot${nc}@${lightblue}Container${nc}:~ "
         read -r cmdtorun
         $proot $console $bash "$cmdtorun"
         runcmd
     }
     function runcmd {
-        printf "${bold}${lightred}vRoot${nc}@${lightblue}Container${nc}:~ "
+        printf "${bold}${lightred}pRoot${nc}@${lightblue}Container${nc}:~ "
         read -r cmdtorun
         $proot $console $bash "$cmdtorun"
         runcmd1
