fd580ca183bf2931ddbb6be46863aa8de7611243
RiNo <75165318+RinoOnTop@users.noreply.github.com>
2023-08-09 13:16:38 +0300

    Update vps1.sh

--- vps1.sh
+++ vps1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-HOME="/home/container"
 $LINUX_ISO = "Alpine"
+HOME="/home/container"
 HOMEA="$HOME/linux/.apt"
 STAR1="$HOMEA/lib:$HOMEA/usr/lib:$HOMEA/var/lib:$HOMEA/usr/lib/x86_64-linux-gnu:$HOMEA/lib/x86_64-linux-gnu:$HOMEA/lib:$HOMEA/usr/lib/sudo"
 STAR2="$HOMEA/usr/include/x86_64-linux-gnu:$HOMEA/usr/include/x86_64-linux-gnu/bits:$HOMEA/usr/include/x86_64-linux-gnu/gnu"
@@ -31,13 +31,30 @@ else
     install="$INSTALL"
 fi
 if [ -z "$LINUX_ISO" ]; then
-   linux_iso="https://github.com/termux/proot-distro/releases/download/v3.3.0/alpine-x86_64-pd-v3.3.0.tar.xz"
-        bash=("/bin/ash -c")
-        if [ $install = "0" ]; then
-        cmds=("apk cache clean" "apk update" "apk upgrade" "apk add --upgrade sudo curl wget hwloc htop nano neofetch python3 unzip")
+    linux_iso="https://github.com/termux/proot-distro/releases/download/v3.3.0/debian-x86_64-pd-v3.3.0.tar.xz"
+    bash=("/bin/bash -c")
+    if [ -z "$INSTALL" ]; then
+        cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "apt clean" "apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3")
+    fi
+else
+    if [ $LINUX_ISO = "Debian" ]; then
+        linux_iso="https://github.com/termux/proot-distro/releases/download/v3.3.0/debian-x86_64-pd-v3.3.0.tar.xz"
+        bash=("/bin/bash -c")
+    if [ $install = "0" ]; then
+        cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "apt clean" "apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3")
         else
-        cmds=("apk cache clean" "apk update" "apk upgrade")
+        cmds=("apt clean" "apt-get update" "apt-get -y upgrade" "apt-get -y install python3")
         fi
+    fi
+    if [ $LINUX_ISO = "Ubuntu" ]; then
+        linux_iso="https://partner-images.canonical.com/core/bionic/current/ubuntu-bionic-core-cloudimg-amd64-root.tar.gz"
+        bash=("/bin/bash -c")
+        if [ $install = "0" ]; then
+        cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "apt clean" "rm -rf /etc/apt/trusted.gpg.d/*" "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32" "apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3")
+        else
+        cmds=("apt clean" "apt-get update" "apt-get -y upgrade" "apt-get -y install python3")
+        fi
+    fi
     if [ $LINUX_ISO = "Alpine" ]; then
         linux_iso="https://github.com/termux/proot-distro/releases/download/v3.3.0/alpine-x86_64-pd-v3.3.0.tar.xz"
         bash=("/bin/ash -c")
@@ -88,6 +105,7 @@ ${bold}${lightred}⛔️  To use this variable, you have to be using docker: ash
         read -r cmdtorun
         $proot $console $bash "$cmdtorun"
         runcmd1
+    }
     runcmd
 else
     echo "${bold}${lightblue}🔎   Architecture: 64x"
